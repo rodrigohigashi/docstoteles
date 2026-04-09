5a31cd461c0249879a38161724c439d6c001da49
Rodrigo Higashi <10561513+rodrigohigashi@users.noreply.github.com>
2026-04-09 09:26:11 -0300

    Atualiza rag e requirements

--- docstoteles/service/rag.py
+++ docstoteles/service/rag.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import DirectoryLoader, TextLoader    # load documents
-from langchain_community.embeddings import FastEmbedEmbeddings                  # embeddings integration
+from langchain_huggingface import HuggingFaceEndpointEmbeddings                  # embeddings integration
 from langchain_community.vectorstores import FAISS                              # vector database
 from langchain.text_splitter import RecursiveCharacterTextSplitter              # split documents into smaller chunks
 from langchain_groq import ChatGroq                                             # Groq API
@@ -10,8 +10,9 @@ from langchain.prompts import PromptTemplate                                    
 class RAGService:
     def __init__(self):
         # Initialize embeddings
-        self.embeddings = FastEmbedEmbeddings(
-            model_name="BAAI/bge-small-en-v1.5"                   # embedding model
+        self.embeddings = HuggingFaceEndpointEmbeddings(
+            model="sentence-transformers/all-MiniLM-L6-v2",                     # embedding model
+            huggingfacehub_api_token=os.getenv("HF_TOKEN")                                      
         )
         
         # Initialize LLM
--- docstoteles/service/rag_pt.py
+++ docstoteles/service/rag_pt.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import DirectoryLoader, TextLoader    # carregar os documentos
-from langchain_community.embeddings import FastEmbedEmbeddings                  # lib de integração
+from langchain_huggingface import HuggingFaceEndpointEmbeddings                 # lib de integração
 from langchain_community.vectorstores import FAISS                              # banco de dados multivetorial - pesos
 from langchain.text_splitter import RecursiveCharacterTextSplitter              # quebra de documentos em micro pedaços
 from langchain_groq import ChatGroq                                             # API da Groq
@@ -10,8 +10,9 @@ from langchain.prompts import PromptTemplate                                    
 class RAGService:
     def __init__(self):
         # Inicializar embeddings
-        self.embeddings = FastEmbedEmbeddings(            
-            model_name="BAAI/bge-small-en-v1.5"                   # modelo self embeddings
+        self.embeddings = HuggingFaceEndpointEmbeddings(
+            model="sentence-transformers/all-MiniLM-L6-v2",                     # embedding model
+            huggingfacehub_api_token=os.getenv("HF_TOKEN")                                      
         )
         
         # Inicializar LLM
